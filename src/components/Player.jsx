cfaaef99987a1ec07f337476600797ea46bf13a0
Ping Zhu <zhuping817@gmail.com>
2024-10-22 10:28:43 +0200

    add game board

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handelEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handelChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handelChange} />
+    );
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handelEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
