04221c04bc198b25f9fff2e1607e64bcdf614ff8
Ping Zhu <zhuping817@gmail.com>
2024-10-22 10:56:09 +0200

    active

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import Player from "./components/Player";
 import GameBoard from "./components/Gameboard";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handelSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="play 1" symbol="X" />
-          <Player initialName="play 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="play 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="play 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handelSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
