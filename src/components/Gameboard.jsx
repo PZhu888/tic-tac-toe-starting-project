3e46f325dac30953c8d0ff92503108979d16a010
Ping Zhu <zhuping817@gmail.com>
2024-10-22 16:48:59 +0200

    add disabled to button

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -30,7 +30,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIdex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIdex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
